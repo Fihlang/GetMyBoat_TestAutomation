3b6eff72b3a631cf5e3a584f2b0fb77744cf37b7
Fihlang <fihlangtsoananyana@gmail.com>
2021-04-08 14:09:06 +0200

    updated configurations

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,7 +16,7 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 
 module.exports = (on, config) => {
 
-  require('@cypress/code-coverage/task')(on, config);
+
 
   on('file:preprocessor', cucumber());
 
